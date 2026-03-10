9dae5c1df07904a18c69cf8bea071c2a7e11a7c0
pratik <pratikkumarpradhan2006@gmail.com>
2026-03-10 21:11:44 +0530

    insurance portion sorted

---
+++ lib/models/insurance_product_request.dart
@@ -0,0 +1,34 @@
+class InsuranceProductRequest {
+  String? sellerId;
+  String? sellerCompanyId;
+  String? packagePurchasedId;
+  String? masterCategoryId;
+  String? vehicleCategoryId;
+  String? vehicleCompanyId;
+  String? vehicleModelTypeId;
+  String? vehicleModelId;
+  String? vehicleYearId;
+  String? productName;
+  String? serialNumber;
+  String? price;
+  String? description;
+  String? productCondition;
+
+  InsuranceProductRequest({
+    this.sellerId,
+    this.sellerCompanyId,
+    this.packagePurchasedId,
+    this.masterCategoryId,
+    this.vehicleCategoryId,
+    this.vehicleCompanyId,
+    this.vehicleModelTypeId,
+    this.vehicleModelId,
+    this.vehicleYearId,
+    this.productName,
+    this.serialNumber,
+    this.price,
+    this.description,
+    this.productCondition,
+  });
+}
+
